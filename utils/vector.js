dca30c2c41f148b4f19ffa2431d8b08e8b1f51dd
banginwar <banginwar.amit@gmail.com>
2025-06-10 22:19:26 +0530

    error resolved

--- utils/vector.js
+++ utils/vector.js
@@ -1,9 +1,9 @@
-export function cosineSimilarity(vecA, vecB) {
-    const a = tf.tensor1d(vecA);
-    const b = tf.tensor1d(vecB);
-    const dotProduct = tf.sum(tf.mul(a, b));
-    const normA = tf.norm(a);
-    const normB = tf.norm(b);
-    const similarity = dotProduct.div(normA.mul(normB));
-    return similarity.arraySync();
-  }
+// export function cosineSimilarity(vecA, vecB) {
+//     const a = tf.tensor1d(vecA);
+//     const b = tf.tensor1d(vecB);
+//     const dotProduct = tf.sum(tf.mul(a, b));
+//     const normA = tf.norm(a);
+//     const normB = tf.norm(b);
+//     const similarity = dotProduct.div(normA.mul(normB));
+//     return similarity.arraySync();
+//   }
