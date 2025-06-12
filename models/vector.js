56089e9b86d964fb519b945d22750a7f033ba1e9
banginwar <banginwar.amit@gmail.com>
2025-06-12 18:36:25 +0530

    added new model for vector

--- models/vector.js
+++ models/vector.js
@@ -1,9 +1,24 @@
+// const tf = require('@tensorflow/tfjs-node');
+// let modelPromise = null;
+
+// function getModel() {
+//   if (!modelPromise) {
+//     modelPromise = tf.loadGraphModel('https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v2_140_224/classification/3/default/1', { fromTFHub: true });
+//   }
+//   return modelPromise;
+// }
+
+// module.exports = getModel;
+
+//Above this is the previous code
+
 const tf = require('@tensorflow/tfjs-node');
 let modelPromise = null;
 
 function getModel() {
   if (!modelPromise) {
-    modelPromise = tf.loadGraphModel('https://tfhub.dev/google/tfjs-model/imagenet/mobilenet_v2_140_224/classification/3/default/1', { fromTFHub: true });
+    modelPromise = tf.loadGraphModel("https://www.kaggle.com/models/google/mobilenet-v2/TfJs/140-224-classification/3", { fromTFHub: true })
+
   }
   return modelPromise;
 }
